4f35dd2471c6edbe993fbe09ecf4861a93f8679f
Kirilenko Danila <33377623+gc-group@users.noreply.github.com>
2018-06-29 19:52:29 +0300

    Add files via upload

---
+++ stall-tkd/scripts/contact.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!empty($_POST['contactname']) && !empty($_POST['contactsurname']) && !empty($_POST['contactemail']) && !empty($_POST['contactphone']) && !empty($_POST['contactmessage'])) {
+	$to = 'kirilenko.danila@gmail.com'; // Your e-mail address here.
+	$body = "\nName: {$_POST['contactname']}\nName: {$_POST['contactsurname']}\nPhone: {$_POST['contactphone']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n";
+	mail($to, "Заявка на запись в таэквондо клуб "СТАЛЬ"! ", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+    }
+}
+?>
