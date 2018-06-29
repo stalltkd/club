f15e4220fddb52e8c50c6d99208a965fd6a9f97d
Kirilenko Danila <33377623+gc-group@users.noreply.github.com>
2018-06-29 20:29:11 +0300

    Update contact.php

--- stall-tkd/scripts/contact.php
+++ stall-tkd/scripts/contact.php
@@ -1,9 +1,25 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactphone']) && !empty($_POST['contact_message'])) {
-	$to = 'kirilenko.danila@gmail.com'; // Your e-mail address here.
-	$body = "\nName: {$_POST['contactname']}\nPhone: {$_POST['contactphone']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n";
-	mail($to, "Заявка на запись в таэквондо клуб "СТАЛЬ"! ", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+    if(!empty($_POST['contactname']) 
+       &&
+       if(!empty($_POST['contactsurname']) 
+       &&
+       if(!empty($_POST['contactemail']) 
+       &&
+	if(!empty($_POST['contactphone']) 
+       &&
+       if(!empty($_POST['contactmessage'])) {
+	    
+	$to = 'danila.kirilenko@hotmail.com'; // Your e-mail address here.
+	$body = "\nName: {$_POST['contactname']}
+		\nEmail: {$_POST['contactemail']}
+		\nEmail: {$_POST['contactphone']}
+	\n
+	\n
+	\n
+	\n
+	{$_POST['contactmessage']}\n\n";
+	mail($to, "Message from yoursite.com", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
     }
 }
 ?>
