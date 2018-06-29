81d1070ec0b94d4320536db2df748d5c5e36bfce
Kirilenko Danila <33377623+gc-group@users.noreply.github.com>
2018-06-29 19:57:39 +0300

    Update contact.php

--- stall-tkd/scripts/contact.php
+++ stall-tkd/scripts/contact.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(!empty($_POST['contactname']) && !empty($_POST['contactsurname']) && !empty($_POST['contactemail']) && !empty($_POST['contactphone']) && !empty($_POST['contactmessage'])) {
+    if(!empty($_POST['contact_name']) && !empty($_POST['contact_surname']) && !empty($_POST['contact_email']) && !empty($_POST['contact_phone']) && !empty($_POST['contact_message'])) {
 	$to = 'kirilenko.danila@gmail.com'; // Your e-mail address here.
 	$body = "\nName: {$_POST['contactname']}\nName: {$_POST['contactsurname']}\nPhone: {$_POST['contactphone']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n";
 	mail($to, "Заявка на запись в таэквондо клуб "СТАЛЬ"! ", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
